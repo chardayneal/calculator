692dd1d38345ecd966db2561fd4935882148542a
chardayneal <106034224+chardayneal@users.noreply.github.com>
2023-04-20 07:43:13 -0700

    Create operate function
    operate() takes in three parameters and returns appropriate function

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+// C A L C U L A T O R   V A R I A B L E S
+let firstNum;
+let operator;
+let secondNum;
+
+// S I M P L E   C A L C U L A T O R   F U N C T I O N S
 const add = function(a, b) {return a + b};
-console.log(add(1,2));
-
 const subtract = function(a, b) {return a - b};
-console.log(subtract(5,2));
-
 const multiply = function(a, b) {return a * b};
-console.log(multiply(1,3));
-
 const divide = function(a, b) {return a / b};
-console.log(divide(6,2));
+
+
+const operate = function(operator, a, b) {
+    switch (operator) {
+        case 'add':
+            console.log(add(a, b));
+            break;
+        case 'subtract':
+            console.log(subtract(a, b));
+            break;
+        case 'multiply':
+            console.log(multiply(a, b));
+            break;
+        case 'divide':
+            console.log(divide(a, b));
+            break;
+    }
+}
+
+operate('subtract', 2, 2);
