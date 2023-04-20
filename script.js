503597623941a424b853a84f7d531611dd9d1dd7
chardayneal <106034224+chardayneal@users.noreply.github.com>
2023-04-20 12:56:48 -0700

    Display selected num button to calculator screen
    Displays number and stores first number onces an operator is selected

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // C A L C U L A T O R   V A R I A B L E S
 let firstNum;
-let operator;
+let operatorSelected;
 let secondNum;
 
 // S I M P L E   C A L C U L A T O R   F U N C T I O N S
@@ -27,4 +27,53 @@ const operate = function(operator, a, b) {
     }
 }
 
-operate('subtract', 2, 2);
+const screenDisplay = document.querySelector('#screen');
+
+const numbers = document.querySelectorAll('.numbers');
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        if (screenDisplay.textContent == 'ERROR') {
+            screenDisplay.textContent = number.textContent;
+        } else {
+            screenDisplay.append(number.textContent);
+        }
+    });
+
+});
+
+const operators = document.querySelectorAll('.operators');
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (screenDisplay.textContent == '') {
+            screenDisplay.textContent = 'ERROR';
+        } else {
+            firstNum = Number(screenDisplay.textContent)
+            operatorSelected = `${operator.id}`;
+            getSecondNumber();
+            console.log(`First Number: ${firstNum}`);
+            console.log(`Operator: ${operatorSelected}`);
+        }
+    });
+});
+
+function getSecondNumber() {
+    numbers.forEach((number) => {
+        number.addEventListener('click', () => {
+
+            screenDisplay.append(number.textContent);
+        });
+    
+    });
+
+    operators.forEach((operator) => {
+        operator.addEventListener('click', () => {
+                firstNum = Number(screenDisplay.textContent)
+                operatorSelected = `${operator.id}`;
+                screenDisplay.textContent = '';
+                getSecondNumber();
+                console.log(`First Number: ${firstNum}`);
+                console.log(`Operator: ${operatorSelected}`);
+            }
+        );
+    })
+}
