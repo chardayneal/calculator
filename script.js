f263c189136fc05bacf2e071c1069f45494388b9
chardayneal <106034224+chardayneal@users.noreply.github.com>
2023-04-20 07:22:37 -0700

    Create four function for calculator, link to html
    created add, subtract, multiply, divide functions
    tested in console
    
    linked js to html file to view in console

---
+++ script.js
@@ -0,0 +1,11 @@
+const add = function(a, b) {return a + b};
+console.log(add(1,2));
+
+const subtract = function(a, b) {return a - b};
+console.log(subtract(5,2));
+
+const multiply = function(a, b) {return a * b};
+console.log(multiply(1,3));
+
+const divide = function(a, b) {return a / b};
+console.log(divide(6,2));
